85524880479f398f6423e3eceaa3d7a62f67a97a
Anthony Gendron <aperturescience540@gmail.com>
2017-10-13 13:57:40 -0400

    Lab_5_1_4

--- main.cpp
+++ main.cpp
@@ -12,14 +12,49 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+class ShopItemOrder
+{
+public:
+    ShopItemOrder(string name, double price, int orderSize);
+    void setName(string name);
+    void setPrice(double price);
+    void setOrderSize(int orderSize);
+    string getOrder();
+private:
+    string name;
+    double price;
+    int orderSize;
+    double orderPrice;
+    string finalOrder;
+};
 
+ShopItemOrder::ShopItemOrder(string name, double price, int orderSize)
+{
+    this->name = name;
+    this->price = price;
+    this->orderSize = orderSize;
+};
+
+string ShopItemOrder::getOrder()
+{
+    this->name = name;
+    this->price = price;
+    this->orderSize = orderSize;
+    this->orderPrice = orderSize * price;
+    this->finalOrder = ("Item = " + name + "\nPrice = $" + to_string(price) + "\nOrder size = " + to_string(orderSize) + "\nFinal price = $" + to_string(orderPrice));
+    return finalOrder;   
+};
+
+int main() 
+{
+    ShopItemOrder order1("Dog chew toys", 4.99, 2);
+    cout << order1.getOrder() << endl;
+    
     return 0;
 }
-
